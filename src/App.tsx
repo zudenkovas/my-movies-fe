57350fd7b438aef20da98647662568ff819cf981
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-09-05 22:31:39 +0300

    Label change

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img alt="logo" className="App-logo" src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Dev Mentor.</p>
         <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
           Learn React
         </a>
