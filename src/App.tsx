880cda194703b13993f0dd698c858527e53f337c
Vitalijus Zudenkovas <vitalijus.zudenkovas@devbridge.com>
2021-09-20 23:11:10 +0300

    Remove redundant requests

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,13 @@ import axios from 'axios';
 import LogoIcon from './components/Icons/LogoIcon';
 import styles from './App.module.css';
 
-async function fetchPosts() {
-  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-  return data;
-}
-
 async function fetchStatus() {
   const { data } = await axios.get('https://api.movies.devmentor.online/prod');
   return data;
 }
 
 function App(): JSX.Element {
-  const { data: posts } = useQuery('posts', fetchPosts);
   const { data: status } = useQuery('status', fetchStatus);
-
-  console.log(posts, status);
 
   return (
     <div className={styles.app}>
@@ -26,13 +18,6 @@ function App(): JSX.Element {
         <LogoIcon />
         <p>My Movies</p>
         {status && <p>Status: {status.message}</p>}
-        {posts && (
-          <ul>
-            {posts.map((post: { title: string }, index: number) => {
-              return <li key={index}>{post.title}</li>;
-            })}
-          </ul>
-        )}
       </header>
     </div>
   );
