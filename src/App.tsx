20ff15b3c79fb2807fc3fd36c05e4197b9955f1f
Vitalijus Zudenkovas <vitalijus.zudenkovas@devbridge.com>
2021-09-29 20:18:50 +0300

    Medium implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
-
-import Layout from './components/Layout';
-import Header from './components/Header';
+import Layout from 'components/Layout';
+import Header from 'components/Header';
 
 async function fetchStatus() {
   const { data } = await axios.get('http://localhost:3001/health');
   return data;
 }
 
+// TODO: Add footer to be shown
 function App(): JSX.Element {
   const { data: healthy } = useQuery('status', fetchStatus);
 
   return (
-    <Layout footer={<footer>Footer</footer>} header={<Header />} sidebar={<div>Sidebar</div>}>
+    <Layout footer={<div>Footer</div>} header={<Header />} sidebar={<div>Sidebar</div>}>
       <p>My Movies</p>
       <p>API Status: {healthy ? 'Is running' : 'Something is wrong!'}</p>
     </Layout>
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+// TODO: Update component according to AC's
+
+const Footer = () => <div>&copy; All rights reserved.</div>;
+
+export default Footer;
