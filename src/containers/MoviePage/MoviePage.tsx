85542aa8a92e02e0c5af59651ce88daaba524aca
Vitalijus Zudenkovas <vitalijus.zudenkovas@vitalijus.zudenkovas.domain>
2021-10-14 23:42:31 +0300

    Create Movie page

--- src/containers/MoviePage/MoviePage.tsx
+++ src/containers/MoviePage/MoviePage.tsx
@@ -2,6 +2,7 @@ import { useLocation, useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getMovie } from 'api/movies/moviesLib';
 import Loader from 'components/Loader';
+import Tag from 'components/Tag';
 
 import styles from './MoviePage.module.css';
 
@@ -26,7 +27,42 @@ export const MoviePage = () => {
             </div>
 
             <div className={styles.movieDataWrapper}>
-              <h2>{data?.title}</h2>I am the movie
+              <div className={styles.movieDataHeader}>
+                <h2>
+                  {data?.title} <span className={styles.movieDataHeaderDate}>{`(${data?.releaseDate})`}</span>
+                </h2>
+
+                <div className={styles.tagList}>
+                  {data?.genres.map(({ id, name }) => (
+                    <Tag key={id}>
+                      <span>{name}</span>
+                    </Tag>
+                  ))}
+                </div>
+
+                <div className={styles.durationContainer}>
+                  <span>Duration: </span> <strong>{data?.runtime} min</strong>
+                </div>
+
+                <div className={styles.voteContainer}>
+                  <span>
+                    Vote average: <strong>{data?.voteAverage} </strong>
+                  </span>
+                  <span>
+                    Vote count: <strong>{data?.voteCount}</strong>
+                  </span>
+                </div>
+
+                <div className={styles.overviewContainer}>
+                  <h3>Overview</h3>
+                  <span>{data?.overview}</span>
+                </div>
+
+                <div className={styles.overviewContainer}>
+                  <h3>Production Companies</h3>
+                  <span>{data?.productionCompanies.map((company) => company.name).join(', ')}</span>
+                </div>
+              </div>
             </div>
           </section>
         </div>
