0f942e2d891ca654f57e7fb89deec748ba91cb49
Vitalijus Zudenkovas <vitalijus.zudenkovas@vitalijus.zudenkovas.domain>
2021-10-15 23:05:40 +0300

    Remove console .logs

--- src/api/shared/methods.ts
+++ src/api/shared/methods.ts
@@ -3,6 +3,5 @@ import axios, { AxiosResponse } from 'axios';
 import { BASE_API_URL } from './constants';
 
 export function get<T>(url: string): Promise<AxiosResponse<T>> {
-  console.log(BASE_API_URL, process.env);
   return axios.get<T>(`${BASE_API_URL}/${url}`);
 }
