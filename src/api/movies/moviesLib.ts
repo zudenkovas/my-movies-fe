2ecc186fc239dfd2f428631112b525fdc8866b3c
Vitalijus Zudenkovas <vitalijus.zudenkovas@vitalijus.zudenkovas.domain>
2021-10-15 23:05:01 +0300

    Fix get methods

--- src/api/movies/moviesLib.ts
+++ src/api/movies/moviesLib.ts
@@ -1,13 +1,12 @@
-import axios from 'axios';
-
 import { MoviesResponse, MovieDetails } from './types';
+import { get } from '../shared/methods';
 
 export async function getMovies(): Promise<MoviesResponse> {
-  const { data } = await axios.get('movies?page=1');
+  const { data } = await get<MoviesResponse>('movies?page=1');
   return data;
 }
 
 export async function getMovie(id: string): Promise<MovieDetails> {
-  const { data } = await axios.get(`movies/${id}`);
+  const { data } = await get<MovieDetails>(`movies/${id}`);
   return data;
 }
