f1ec45ace15969c2a2a24c09b5f4f6430874df3d
Vitalijus Zudenkovas <vitalijus.zudenkovas@vitalijus.zudenkovas.domain>
2021-10-15 22:05:37 +0300

    Minor fixes

--- src/api/movies/moviesLib.ts
+++ src/api/movies/moviesLib.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 import { MoviesResponse, MovieDetails } from './types';
 
 export async function getMovies(): Promise<MoviesResponse> {
-  const { data } = await axios.get('/movies?page=1');
+  const { data } = await axios.get('movies?page=1');
   return data;
 }
 
 export async function getMovie(id: string): Promise<MovieDetails> {
-  const { data } = await axios.get(`/movies/${id}`);
+  const { data } = await axios.get(`movies/${id}`);
   return data;
 }
